6ac499e6e7698261a7ddd87fe5b63916a806d1e9
KattoDev <alexisrincon27@outlook.com>
2025-02-19 13:52:51 -0500

    Add WIP sidebar management module

---
+++ app/leapforce/src/app/components/sidebar/management-module/management-module.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-module',
+  imports: [],
+  templateUrl: './management-module.component.html',
+  styleUrl: './management-module.component.css'
+})
+export class ManagementModuleComponent {
+
+}
