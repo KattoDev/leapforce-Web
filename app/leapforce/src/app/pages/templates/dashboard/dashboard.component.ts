0c01baac748fe5a5163d6df936373584860fba99
Katto <154762212+KattoDev@users.noreply.github.com>
2025-02-21 19:00:34 -0500

    Modify dashboard
    setting to a template

---
+++ app/leapforce/src/app/pages/templates/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [SidebarComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {}
