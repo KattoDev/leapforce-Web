1aff0b4b6c371fcb19cfb2095fe88acb4ecbb99e
Katto <154762212+KattoDev@users.noreply.github.com>
2025-02-23 23:32:39 -0500

    Modify Dashboard view

--- app/LeapforceWebApp/src/views/DashboardView.vue
+++ app/LeapforceWebApp/src/views/DashboardView.vue
@@ -1,18 +1,39 @@
-<script setup></script>
+<script setup>
+import TaskSimpleCardComponent from '@/components/dashboard/TaskSimpleCardComponent.vue'
+import TeamSimpleCardComponent from '@/components/dashboard/TeamSimpleCardComponent.vue'
+import UserSimpleCardComponent from '@/components/dashboard/UserSimpleCardComponent.vue'
+import SidebarComponent from '@/components/sidebar/SidebarComponent.vue'
+</script>
 
 <template>
   <div class="dashboard-container">
-    <div class="sidebar"></div>
-    <div class="content"></div>
+    <div class="sidebar">
+      <SidebarComponent />
+    </div>
+    <div class="content">
+      <UserSimpleCardComponent class="card" />
+      <div id="frame">
+        <TaskSimpleCardComponent class="card" />
+        <TeamSimpleCardComponent class="card" />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.sidebar {
-  background-color: red;
+.sidebar,
+.content {
+  border-radius: 10px;
 }
 
 .content {
-  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  #frame {
+    display: flex;
+    gap: 10px;
+  }
 }
 </style>
