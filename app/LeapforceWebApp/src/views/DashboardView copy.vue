d01a6d15daac66502a9b05a2dd73e1b0da20ac60
Katto <154762212+KattoDev@users.noreply.github.com>
2025-02-23 23:32:54 -0500

    Add dashboard template

---
+++ app/LeapforceWebApp/src/views/DashboardView copy.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SidebarComponent from '@/components/sidebar/SidebarComponent.vue'
+</script>
+
+<template>
+  <div class="dashboard-container">
+    <div class="sidebar">
+      <SidebarComponent />
+    </div>
+    <div class="content"></div>
+  </div>
+</template>
+
+<style scoped>
+.sidebar,
+.content {
+  border-radius: 10px;
+}
+</style>
