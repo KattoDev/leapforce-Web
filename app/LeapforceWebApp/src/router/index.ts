3089bccfd450256fa73062567442c19f0b61f6cd
KattoDev <alexisrincon27@outlook.com>
2025-03-02 15:34:20 -0500

    Add WIP UserProfileView

--- app/LeapforceWebApp/src/router/index.ts
+++ app/LeapforceWebApp/src/router/index.ts
@@ -1,5 +1,6 @@
 import DashboardView from '@/views/DashboardView.vue'
 import LoginView from '@/views/LoginView.vue'
+import UserProfileView from '@/views/UserProfileView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+    },
+    {
+      path: '/dashboard/profile',
+      name: 'user-profile',
+      component: UserProfileView,
     },
   ],
 })
