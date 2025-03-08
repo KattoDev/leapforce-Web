27ddf2b132ac7e8f532182609b9a3c7e552e2f74
KattoDev <alexisrincon27@outlook.com>
2025-03-07 23:43:54 -0500

    Responses refactor and app routing

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,10 @@ import cors from "cors";
 import morgan from "morgan";
 
 import appConfig from "./appConfig";
+
 import USERS_ROUTING from "./endpoints/users/router";
 import TEAMS_ROUTING from "./endpoints/teams/router";
 import PROJECTS_ROUTING from "./endpoints/projects/router";
-import REPORTS_ROUTING from "./endpoints/reports/router";
 import TASKS_ROUTING from "./endpoints/tasks/router";
 
 require("dotenv").config();
@@ -18,7 +18,7 @@ app.use(
   //CORS
   cors({
     origin: process.env.FRONTEND,
-    methods: ["GET, POST"],
+    methods: ["GET, POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type"],
   }),
   // MIDDLEWARE
@@ -34,7 +34,6 @@ app.set("port", appConfig.app.port);
 app.use("/users", USERS_ROUTING);
 app.use("/teams", TEAMS_ROUTING);
 app.use("/projects", PROJECTS_ROUTING);
-app.use("/reports", REPORTS_ROUTING);
 app.use("/tasks", TASKS_ROUTING);
 
 export default app;
